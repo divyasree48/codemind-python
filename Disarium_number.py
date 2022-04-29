0b3f286c6fd626f084d7cdeb6a93793a2431a62e
Divya <divyasreem48@gmail.com>
2022-04-29 21:42:35 +0530

    import math n=int(input()) temp=n rev=0 s=0 d=0 while(n):     r=n%10     n=n//10     rev=rev*10+r while(rev):     d=d+1     r=rev%10     rev=rev//10     s=s+pow(r,d) if(s==temp):     print('True') else:     print('False')

---
+++ Disarium_number.py
@@ -0,0 +1,21 @@
+import math
+n=int(input())
+temp=n
+rev=0
+s=0
+d=0
+while(n):
+    r=n%10
+    n=n//10
+    rev=rev*10+r
+while(rev):
+    d=d+1
+    r=rev%10
+    rev=rev//10
+    s=s+pow(r,d)
+if(s==temp):
+    print('True')
+else:
+    print('False')
+    
+    

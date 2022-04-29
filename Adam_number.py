529dedd0e95b9793f1d6247620f4341d969f8580
Divya <divyasreem48@gmail.com>
2022-04-29 23:40:10 +0530

    n=int(input()) sq=n*n temp=sq rev=0 while(n):     r=n%10     n=n//10     rev=rev*10+r sqrev=rev*rev ss=0 while(sqrev):     r1=sqrev%10     sqrev=sqrev//10     ss=ss*10+r1 if(ss==sq):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,18 @@
+n=int(input())
+sq=n*n
+temp=sq
+rev=0
+while(n):
+    r=n%10
+    n=n//10
+    rev=rev*10+r
+sqrev=rev*rev
+ss=0
+while(sqrev):
+    r1=sqrev%10
+    sqrev=sqrev//10
+    ss=ss*10+r1
+if(ss==sq):
+    print("True")
+else:
+    print("False")

19dbec29422842db2e4f18e863633a3e5028d155
Divya <divyasreem48@gmail.com>
2022-04-30 11:21:35 +0530

    import math n=int(input()) t=n sq=n*n dc=0 while(t):     t=t//10     dc+=1 pw=pow(10,dc) s=sq%pw if(s==n):     print('Automorphic Number') else:     print("Not an Automorphic Number")

---
+++ Automorphic_Number.py
@@ -0,0 +1,14 @@
+import math
+n=int(input())
+t=n
+sq=n*n
+dc=0
+while(t):
+    t=t//10
+    dc+=1
+pw=pow(10,dc)
+s=sq%pw
+if(s==n):
+    print('Automorphic Number')
+else:
+    print("Not an Automorphic Number")

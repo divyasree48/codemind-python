e39c79e4c4cd8e602d59e32a8f882e80593c12dd
Divya <divyasreem48@gmail.com>
2022-05-26 06:24:00 +0530

    def nprime(n):     c=0     for i in range(1,n+1):         if(n%i==0):             fc=0             for j in range(1,i+1):                 if(i%j==0):                     fc+=1             if fc!=2:                 c+=1     return c n=int(input()) print(nprime(n))

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -0,0 +1,13 @@
+def nprime(n):
+    c=0
+    for i in range(1,n+1):
+        if(n%i==0):
+            fc=0
+            for j in range(1,i+1):
+                if(i%j==0):
+                    fc+=1
+            if fc!=2:
+                c+=1
+    return c
+n=int(input())
+print(nprime(n))

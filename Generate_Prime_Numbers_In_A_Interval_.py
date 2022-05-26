5664ad2218fa3683e7173dc96ad13075426049ff
Divya <divyasreem48@gmail.com>
2022-05-26 06:34:28 +0530

    n=int(input()) m=int(input()) if n>m:     n,m=m,n for i in range(n,m+1):     if(i>1):
            for j in range(2,i):
                if(i%j==0):
                    break
            else:
                print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,12 @@
+n=int(input())
+m=int(input())
+if n>m:
+    n,m=m,n
+for i in range(n,m+1):
+    if(i>1):
+        
+        for j in range(2,i):
+            if(i%j==0):
+                break
+        else:
+            print(i)

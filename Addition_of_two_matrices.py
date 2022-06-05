f96578655f885d11da40395ad7088bde47fc9cba
Divya <divyasreem48@gmail.com>
2022-06-05 19:17:00 +0530

    n=int(input()) a=[list(map(int,input().split()))for i in range(n)] b=[list(map(int,input().split()))for i in range(n)] for i in range(n):     l=[]     for j in range(n):         l.append(a[i][j]+b[i][j])     print(*l)

---
+++ Addition_of_two_matrices.py
@@ -0,0 +1,10 @@
+n=int(input())
+a=[list(map(int,input().split()))for i in range(n)]
+b=[list(map(int,input().split()))for i in range(n)]
+for i in range(n):
+    l=[]
+    for j in range(n):
+        l.append(a[i][j]+b[i][j])
+    print(*l)
+    
+        

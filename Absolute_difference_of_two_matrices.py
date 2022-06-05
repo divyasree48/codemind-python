0d9eb00eafd6647f52cd73be496c1501f9d4dbbf
Divya <divyasreem48@gmail.com>
2022-06-05 19:20:18 +0530

    n=int(input()) a=[list(map(int,input().split()))for i in range(n)] b=[list(map(int,input().split()))for i in range(n)] for i in range(n):     l=[]     for j in range(n):         if(a[i][j]>b[i][j]):             l.append(a[i][j]-b[i][j])         else:             l.append(b[i][j]-a[i][j])     print(*l)

---
+++ Absolute_difference_of_two_matrices.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=[list(map(int,input().split()))for i in range(n)]
+b=[list(map(int,input().split()))for i in range(n)]
+for i in range(n):
+    l=[]
+    for j in range(n):
+        if(a[i][j]>b[i][j]):
+            l.append(a[i][j]-b[i][j])
+        else:
+            l.append(b[i][j]-a[i][j])
+    print(*l)
+    
+        

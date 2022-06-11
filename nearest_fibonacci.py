e12d9946e7ece6252fbebf3273a98d7c1acbae96
Divya <divyasreem48@gmail.com>
2022-06-11 23:15:04 +0530

    def fun(n):     a=0     b=1     c=a+b     i=3     arr=[]     while(c<n):         k=c         a=b         b=c         c=a+b     arr.append(k)     arr.append(c)     return arr n=int(input()) p=[] p=fun(n) if(n-p[0]==p[1]-n):     print(p[0],p[1]) elif(n-p[0]>p[1]-n):     print(p[1]) elif(n-p[0]<p[1]-n):     print(p[0])

---
+++ nearest_fibonacci.py
@@ -0,0 +1,24 @@
+def fun(n):
+    a=0
+    b=1
+    c=a+b
+    i=3
+    arr=[]
+    while(c<n):
+        k=c
+        a=b
+        b=c
+        c=a+b
+    arr.append(k)
+    arr.append(c)
+    return arr
+n=int(input())
+p=[]
+p=fun(n)
+if(n-p[0]==p[1]-n):
+    print(p[0],p[1])
+elif(n-p[0]>p[1]-n):
+    print(p[1])
+elif(n-p[0]<p[1]-n):
+    print(p[0])
+    

1b0fda1585875675708dd34953f964752d778818
Divya <divyasreem48@gmail.com>
2022-06-20 19:39:51 +0530

    n=int(input()) arr=list(map(int,input().split())) s1=0 s2=0 for i in range(n):     if(arr[i]%2==0):         s1+=arr[i]     else:         s2+=arr[i] if(s1>s2):     print(s1-s2) else:     print(s2-s1)

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+s1=0
+s2=0
+for i in range(n):
+    if(arr[i]%2==0):
+        s1+=arr[i]
+    else:
+        s2+=arr[i]
+if(s1>s2):
+    print(s1-s2)
+else:
+    print(s2-s1)

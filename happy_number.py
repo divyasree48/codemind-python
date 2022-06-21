4da1228a45db4f588191be22d1f3398f06d7cc73
Divya <divyasreem48@gmail.com>
2022-06-21 07:12:26 +0530

    def ishappy(res):     sum=rem=0     while res>0:         rem=res%10         sum+=(rem*rem)         res=res//10     return sum n=int(input()) res=n while(res!=1 and res!=4):     res=ishappy(res) if(res==1):     print('True') else:     print('False')

--- happy_number.py
+++ happy_number.py
@@ -2,14 +2,14 @@ def ishappy(res):
     sum=rem=0
     while res>0:
         rem=res%10
-        sum=sum+(rem*rem)
+        sum+=(rem*rem)
         res=res//10
     return sum
 n=int(input())
 res=n
-while res!=1 and res!=4:
+while(res!=1 and res!=4):
     res=ishappy(res)
-if res==1:
+if(res==1):
     print('True')
 else:
     print('False')

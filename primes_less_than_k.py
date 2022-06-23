30b73ecab01d2a03cae1ce61f86263ad0bcdbd42
Divya <divyasreem48@gmail.com>
2022-06-23 07:56:30 +0530

    def fun(arr,k):     c=0     for i in range(len(arr)):         for j in range(2,arr[i]):             if(arr[i]%j==0):                 break         else:             if(arr[i]!=1 and arr[i]<=k):                 c+=1     print(c) n=int(input()) arr=list(map(int,input().split())) k=int(input()) fun(arr,k)

---
+++ primes_less_than_k.py
@@ -0,0 +1,14 @@
+def fun(arr,k):
+    c=0
+    for i in range(len(arr)):
+        for j in range(2,arr[i]):
+            if(arr[i]%j==0):
+                break
+        else:
+            if(arr[i]!=1 and arr[i]<=k):
+                c+=1
+    print(c)
+n=int(input())
+arr=list(map(int,input().split()))
+k=int(input())
+fun(arr,k)

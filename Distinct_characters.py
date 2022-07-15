5df41cca160f0664327865c3c37612f772fb7ead
Divya <divyasreem48@gmail.com>
2022-07-15 21:34:08 +0530

    s=input().lower() s=sorted(set(s)) s1='' for i in s:     if i==' ':         continue     else:         s1+=i print(s1)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,17 +1,9 @@
 s=input().lower()
-l=[]
-c=0
+s=sorted(set(s))
+s1=''
 for i in s:
     if i==' ':
         continue
-    if s.count(i)==1:
-        if i not in l:
-            l.append(i)
-            c+=1
-s1=''
-for i in sorted(l):
-    s1+=i
-if c==0:
-    print(-1)
-else:
-    print(s1)
+    else:
+        s1+=i
+print(s1)

367a08fcf90f002cbca8a7caf472b78ddd03338d
Divya <divyasreem48@gmail.com>
2022-07-15 20:27:07 +0530

    s1=input() s='' for i in s1.split(' '):     s=s+i
    print(min(s),s1.count(min(s)),max(s),s1.count(max(s)))

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,6 +1,6 @@
 s1=input()
-l=list(s1.split(' '))
 s=''
-for i in l:
-    s+=i
-print(min(s),s.count(min(s)),max(s),s.count(max(s)))
+for i in s1.split(' '):
+    s=s+i
+        
+print(min(s),s1.count(min(s)),max(s),s1.count(max(s)))

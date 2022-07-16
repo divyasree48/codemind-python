c0c0352e7ee9bf15e7db7c67c211f5fadfd02fcc
Divya <divyasreem48@gmail.com>
2022-07-16 07:04:41 +0530

    s1=input().lower() s2=input().lower() l=[] for i in s1.split(' '):     if i in s2.split(' '):         if i==' ':             continue         if i not in l:             l.append(i) for i in s2.split(' '):     if i in s1.split(' '):         if i==' ':             continue         if i not in l:             l.append(i) print(len(l))

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,20 +1,16 @@
 s1=input().lower()
 s2=input().lower()
 l=[]
-c=0
 for i in s1.split(' '):
     if i in s2.split(' '):
         if i==' ':
             continue
         if i not in l:
             l.append(i)
-            c+=1
 for i in s2.split(' '):
     if i in s1.split(' '):
         if i==' ':
             continue
         if i not in l:
             l.append(i)
-            c+=1
-
-print(c)
+print(len(l))

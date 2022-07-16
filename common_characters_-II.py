db2cc75dce987a4927c0842b7aeeb1af022d06c1
Divya <divyasreem48@gmail.com>
2022-07-16 06:43:25 +0530

    s1=input().lower() s2=input().lower() l=[] for i in s1:     if i in s2:         if i==' ':             continue         if i not in l:             l.append(i) for i in s2:     if i in s1:         if i==' ':             continue         if i not in l:             l.append(i) print(len(l))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -1,20 +1,16 @@
 s1=input().lower()
 s2=input().lower()
-c=0
 l=[]
 for i in s1:
-    if i==' ':
-        continue
     if i in s2:
+        if i==' ':
+            continue
         if i not in l:
             l.append(i)
-            c+=1
 for i in s2:
-    if i==' ':
-        continue
     if i in s1:
+        if i==' ':
+            continue
         if i not in l:
             l.append(i)
-            c+=1
-
-print(c)
+print(len(l))

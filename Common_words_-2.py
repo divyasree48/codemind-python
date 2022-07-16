3e24a43d33d5b1eca4cd24a5f3dec45d392780bf
Divya <divyasreem48@gmail.com>
2022-07-16 07:09:40 +0530

    s1=input().lower() s2=input().lower() l=[] for i in s1.split(' '):     if i in s2.split(' '):         if s1.count(i)==1 and s2.count(i)==1:             if i==' ':                 continue             if i not in l:                 l.append(i) for i in s2.split(' '):     if i in s1.split(' '):         if s1.count(i)==1 and s2.count(i)==1:             if i==' ':                 continue             if i not in l:                 l.append(i) print(len(l))

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -1,7 +1,6 @@
 s1=input().lower()
 s2=input().lower()
 l=[]
-c=0
 for i in s1.split(' '):
     if i in s2.split(' '):
         if s1.count(i)==1 and s2.count(i)==1:
@@ -9,7 +8,6 @@ for i in s1.split(' '):
                 continue
             if i not in l:
                 l.append(i)
-                c+=1
 for i in s2.split(' '):
     if i in s1.split(' '):
         if s1.count(i)==1 and s2.count(i)==1:
@@ -17,6 +15,4 @@ for i in s2.split(' '):
                 continue
             if i not in l:
                 l.append(i)
-                c+=1
-
-print(c)
+print(len(l))

4c3f3c12136c2bc86be97c62f245feb6613cc321
Divya <divyasreem48@gmail.com>
2022-07-17 08:07:25 +0530

    s=input() v='aeiouAEIOU' l=[] c=0 for i in s:     if i==' ':         continue     if i in v:         if i not in l:             l.append(i)             c+=1 if c==0:     print(-1) else:     print(*l,end=' ')

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,12 +1,15 @@
 s=input()
 v='aeiouAEIOU'
 l=[]
+c=0
 for i in s:
+    if i==' ':
+        continue
     if i in v:
         if i not in l:
             l.append(i)
-k=len(l)
-if k>0:
-    print(*l)
-else:
+            c+=1
+if c==0:
     print(-1)
+else:
+    print(*l,end=' ')

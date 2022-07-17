6d5d2aa827e97f65ae41386d2a0b76d834c4712d
Divya <divyasreem48@gmail.com>
2022-07-17 09:35:49 +0530

    s=input() l=list(s.split(' ')) l=sorted(l) print(*l)

--- sort_the_string_with_words.py
+++ sort_the_string_with_words.py
@@ -1,9 +1,4 @@
 s=input()
-l=list(sorted(s.split(' ')))
-s1=''
-for i in range(len(l)):
-    if(i==len(l)-1):
-        s1+=l[i]
-    else:
-        s1+=l[i]+' '
-print(s1)
+l=list(s.split(' '))
+l=sorted(l)
+print(*l)

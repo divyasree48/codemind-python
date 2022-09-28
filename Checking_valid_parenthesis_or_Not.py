15f3eaaa1b69c3ca3d84f2c0a6f00c613ed91fa7
Divya <divyasreem48@gmail.com>
2022-09-28 11:11:40 +0530

    s=input() st=[] st2=[] k=0 for i in s:     if i=='(':         st.append(i)     elif i=='*':         st2.append(i)     else:         if len(st)==0:             if len(st2)==0:                 k=1                 break             else:                 st2.pop()         else:             st.pop() #print(st) #print(st2) if len(st)==0 and k==0:     print('True') else:     print('False')

---
+++ Checking_valid_parenthesis_or_Not.py
@@ -0,0 +1,24 @@
+s=input()
+st=[]
+st2=[]
+k=0
+for i in s:
+    if i=='(':
+        st.append(i)
+    elif i=='*':
+        st2.append(i)
+    else:
+        if len(st)==0:
+            if len(st2)==0:
+                k=1
+                break
+            else:
+                st2.pop()
+        else:
+            st.pop()
+#print(st)
+#print(st2)
+if len(st)==0 and k==0:
+    print('True')
+else:
+    print('False')

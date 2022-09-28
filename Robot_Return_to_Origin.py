84ba91563105c2b81e30648cd07fd9ca66ce3c33
Divya <divyasreem48@gmail.com>
2022-09-28 21:01:53 +0530

    s=input() u=d=l=r=0 for i in s:     if i=='U':         u+=1     elif i=='D':         d+=1     elif i=='L':         l+=1     elif i=='R':         r+=1 if l==r and u==d:     print('True') else:     print('False')

---
+++ Robot_Return_to_Origin.py
@@ -0,0 +1,15 @@
+s=input()
+u=d=l=r=0
+for i in s:
+    if i=='U':
+        u+=1
+    elif i=='D':
+        d+=1
+    elif i=='L':
+        l+=1
+    elif i=='R':
+        r+=1
+if l==r and u==d:
+    print('True')
+else:
+    print('False')

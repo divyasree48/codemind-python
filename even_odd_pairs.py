e4b0feafd701543879d3ab57041a8ab711e2a05e
Divya <divyasreem48@gmail.com>
2022-10-05 19:09:19 +0530

    n=int(input()) l=list(map(int,input().split())) if n%2==1:     a=(n+1)//2 else:     a=n//2 s=[] e=[] o=[] for i in l:     if i%2==0:         e.append(i)     else:         o.append(i) i=j=0 while(i<len(e) and j<len(o)):     s.append(e[i])     s.append(o[j])     i+=1     j+=1 while(i<len(e)):     s.append(e[i])     i+=1 while(j<len(o)):     s.append(o[j])     j+=1 if len(s)%2==1:     s.append(0) print(*s)

---
+++ even_odd_pairs.py
@@ -0,0 +1,29 @@
+n=int(input())
+l=list(map(int,input().split()))
+if n%2==1:
+    a=(n+1)//2
+else:
+    a=n//2
+s=[]
+e=[]
+o=[]
+for i in l:
+    if i%2==0:
+        e.append(i)
+    else:
+        o.append(i)
+i=j=0
+while(i<len(e) and j<len(o)):
+    s.append(e[i])
+    s.append(o[j])
+    i+=1
+    j+=1
+while(i<len(e)):
+    s.append(e[i])
+    i+=1
+while(j<len(o)):
+    s.append(o[j])
+    j+=1
+if len(s)%2==1:
+    s.append(0)
+print(*s)

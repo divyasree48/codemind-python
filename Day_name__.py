40ee2c0a2134f1246fcd0baf7cc45f16050c9590
Divya <divyasreem48@gmail.com>
2023-01-18 09:29:35 +0530

    n=int(input()) if n==1:     print('monday') elif n==2:     print('tuesday') elif n==3:     print('wednesday') elif n==3:     print('thursday') elif n==4:     print('friday') elif n==5:     print('saturday') elif n==7:     print('sunday') else:     print('invalid input')

---
+++ Day_name__.py
@@ -0,0 +1,17 @@
+n=int(input())
+if n==1:
+    print('monday')
+elif n==2:
+    print('tuesday')
+elif n==3:
+    print('wednesday')
+elif n==3:
+    print('thursday')
+elif n==4:
+    print('friday')
+elif n==5:
+    print('saturday')
+elif n==7:
+    print('sunday')
+else:
+    print('invalid input')

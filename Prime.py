584222fddf2ed81227454c7a3bdc5ff9c6ae6b8d
Divya <divyasreem48@gmail.com>
2023-01-18 12:39:32 +0530

    import math n=int(input()) c=1 for i in range(1,int(pow(n,0.5))+1):     if n%i==0:         c+=1 if c==2:     print('Prime') else:     print('Not Prime')

--- Prime.py
+++ Prime.py
@@ -1,9 +1,10 @@
+import math
 n=int(input())
-c=0
-for i in range(1,n+1):
+c=1
+for i in range(1,int(pow(n,0.5))+1):
     if n%i==0:
         c+=1
-if(c==2):
-    print("Prime")
+if c==2:
+    print('Prime')
 else:
-    print("Not Prime")
+    print('Not Prime')

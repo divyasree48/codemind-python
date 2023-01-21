2702562aac6fcfed636875d8ccfb404320340e6a
Divya <divyasreem48@gmail.com>
2023-01-21 21:25:55 +0530

    a=int(input()) b=int(str(a)[::-1]) c=str(a*a) d=str(b*b) if c==d[::-1]:     print('True') else:     print('False')

--- Adam_number.py
+++ Adam_number.py
@@ -1,18 +1,8 @@
-n=int(input())
-sq=n*n
-temp=sq
-rev=0
-while n:
-    r=n%10
-    n=n//10
-    rev=rev*10+r
-sqrev=rev*rev
-ss=0
-while sqrev:
-    r1=sqrev%10
-    sqrev//=10
-    ss=ss*10+r1
-if(ss==sq):
+a=int(input())
+b=int(str(a)[::-1])
+c=str(a*a)
+d=str(b*b)
+if c==d[::-1]:
     print('True')
 else:
     print('False')

c5f57c3725327a9f39f9e389eabc73904fb408e8
Divya <divyasreem48@gmail.com>
2023-01-22 09:27:10 +0530

    n=int(input()) l=list(map(int,input().split())) a=l[0] b=l[1] l.append(a) l.append(b) c=0 for i in range(1,len(l)-1):     if l[i-1]%2==l[i+1]%2:         continue     else:         c+=1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -1,11 +1,13 @@
 n=int(input())
 l=list(map(int,input().split()))
-k=l[0]
+a=l[0]
 b=l[1]
-l.append(k)
+l.append(a)
 l.append(b)
 c=0
 for i in range(1,len(l)-1):
-    if abs((l[i-1]%2)-(l[i+1]%2))==1:
+    if l[i-1]%2==l[i+1]%2:
+        continue
+    else:
         c+=1
 print(c)

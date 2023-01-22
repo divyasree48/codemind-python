a94459f055a857310025886aa8d71980ad01c652
Divya <divyasreem48@gmail.com>
2023-01-22 10:56:26 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 d=0 for i in l[::-1]:     d+=(2**c)*i     c+=1 print(d)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,8 +1,8 @@
 n=int(input())
-arr=list(map(int,input().split()))
-s=0
-k=len(arr)-1
-for i in range(len(arr)):
-    s+=((2**k)*arr[i])
-    k-=1
-print(s)
+l=list(map(int,input().split()))
+c=0
+d=0
+for i in l[::-1]:
+    d+=(2**c)*i
+    c+=1
+print(d)

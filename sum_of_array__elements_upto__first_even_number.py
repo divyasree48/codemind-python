7b9b3eb99fede1001cb7125fb9ef6cb44fcafbde
Divya <divyasreem48@gmail.com>
2023-01-28 07:21:21 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if i%2==0:         break     else:         c+=i print(c)

--- sum_of_array__elements_upto__first_even_number.py
+++ sum_of_array__elements_upto__first_even_number.py
@@ -1,9 +1,9 @@
 n=int(input())
-arr=list(map(int,input().split()))
-s=0
-for i in arr:
+l=list(map(int,input().split()))
+c=0
+for i in l:
     if i%2==0:
         break
     else:
-        s+=i
-print(s)
+        c+=i
+print(c)
